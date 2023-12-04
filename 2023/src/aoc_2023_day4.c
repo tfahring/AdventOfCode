c0a4d6bcdffee297bdf4b42a9d30430bba5840c6
Tim Fahringer <timfahringer@gmail.com>
2023-12-04 00:41:57 -0500

    day4 cleaned

--- 2023/src/aoc_2023_day4.c
+++ 2023/src/aoc_2023_day4.c
@@ -1,52 +1,3 @@
-static void 
-day4_part1(MemoryArena *arena) {
-    String8 file = os_file_read_all(arena, string8_from_cstr("day4.dat"));
-    u8 lineSplitChar[1] = {'\n'};
-    u8 cardSplitChar[2] = {':','|'};
-    u8 numSplitChar[1]  = {' '};
-
-    String8List lines = string8_split(arena, file, lineSplitChar, 1);
-
-    u64 sum = 0;
-    u64 *winningNumbers = NULL;
-    u64 *myNumbers      = NULL;
-    for (String8ListNode *line = lines.first; line; line = line->next) {
-        String8List card = string8_split(arena, line->string, cardSplitChar, 2);
-
-        String8 cardID               = card.first->string;
-        String8 winningNumbersString = card.first->next->string;
-        String8 myNumbersString      = card.last->string;
-
-        String8List winningNumbersStringList = string8_split(arena, winningNumbersString, numSplitChar, 1);
-        String8List myNumbersStringList      = string8_split(arena, myNumbersString, numSplitChar, 1);
-
-        if (winningNumbers == NULL) {winningNumbers = mem_arena_push(arena, winningNumbersStringList.nodeCount, u64);}
-        if (myNumbers == NULL)      {myNumbers      = mem_arena_push(arena, myNumbersStringList.nodeCount, u64);}
-
-        u64 idx = 0;
-        for (String8ListNode *node = winningNumbersStringList.first; node; node = node->next) {
-            winningNumbers[idx++] = string_to_int64(node->string.buffer, node->string.length);
-        }
-
-        idx = 0;
-        for (String8ListNode *node = myNumbersStringList.first; node; node = node->next) {
-            myNumbers[idx++] = string_to_int64(node->string.buffer, node->string.length);
-        }
-
-        u64 cardSum = 0;
-        for (u64 myNumIdx = 0; myNumIdx < myNumbersStringList.nodeCount; ++myNumIdx) {
-            for (u64 winNumIdx = 0; winNumIdx < winningNumbersStringList.nodeCount; ++winNumIdx) {
-                if (myNumbers[myNumIdx] == winningNumbers[winNumIdx]) {
-                    cardSum++;
-                }
-            }
-        }
-        sum += (1LL << (cardSum-1));
-    }
-
-    printf("%lld\n", sum);
-}
-
 static u64 
 process_card(String8 line) {
     MemoryArena scratch = mem_arena_get_scratch();
@@ -90,12 +41,24 @@ process_card(String8 line) {
 }
 
 static void 
+day4_part1(MemoryArena *arena) {
+    String8 file = os_file_read_all(arena, string8_from_cstr("day4.dat"));
+    u8 lineSplitChar[1] = {'\n'};
+    String8List lines = string8_split(arena, file, lineSplitChar, 1);
+
+    u64 sum = 0;
+    for (String8ListNode *line = lines.first; line; line = line->next) {
+        u64 cardSum = process_card(line->string);
+        sum += (1LL << (cardSum-1));
+    }
+
+    printf("%lld\n", sum);
+}
+
+static void 
 day4_part2(MemoryArena *arena) {
     String8 file = os_file_read_all(arena, string8_from_cstr("day4.dat"));
     u8 lineSplitChar[1] = {'\n'};
-    u8 cardSplitChar[2] = {':','|'};
-    u8 numSplitChar[1]  = {' '};
-
     String8List lines = string8_split(arena, file, lineSplitChar, 1);
 
     u64 *nMatchingNumbers = mem_arena_push_and_zero(arena, lines.nodeCount, u64);
